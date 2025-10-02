ba62656ee3b04a4360d0832c23a633acf7e0f458
Developer Nahida <ummenahida20@gmail.com>
2025-10-02 12:00:23 +0600

    create check auth and protect route

---
+++ src/app/utils/checkAuth.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { verifyToken } from "./jwt";
+import { JwtPayload } from "jsonwebtoken";
+import AppError from "../errorHandler/appErrors";
+import { envVars } from "../config/env";
+
+interface IVerifyUser{
+    id:string,
+    email:string
+}
+
+export const checkAuth = (...restRoles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // console.log("...restRoles",...restRoles)
+        const accessToken = req?.cookies?.accessToken || req?.cookies?.refreshToken || req.headers.authorization;
+        // console.log("accessToken",accessToken)
+        if (!accessToken) throw new AppError(403, "token isn't available");
+        
+        const tokenVarify = verifyToken(accessToken, envVars.JWT_SECRET) as JwtPayload
+        console.log("tokenVarify", tokenVarify)
+        console.log("restRoles", restRoles)
+
+        if (!restRoles.includes(tokenVarify.role)) {
+            throw new AppError(403, "you are not allowed to access this route")
+        }
+        (req as any).verifyUser = tokenVarify as IVerifyUser;
+        next()
+    } catch (err) {
+        next(err)
+    }
+
+}
